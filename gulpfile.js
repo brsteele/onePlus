c9a91bcce4cd82399218fa41ecdae2118bb287cf
Brian Steele <brian@one-pl.us>
2015-09-07 15:32:29 -0500

    added dev task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,12 @@ var gulp = require('gulp'),
 	browserSync = require('browser-sync'),
 	sass = require('gulp-sass'),
 	maps = require('gulp-sourcemaps'),
-	jshint = require('gulp-jshint');
+	jshint = require('gulp-jshint'),
+	del = require('del'),
+	useref = require('gulp-useref'),
+	iff = require('gulp-if'),
+	uglify = require('gulp-uglify'),
+	csso = require('gulp-csso');
 
 var options = {
 	src : 'src',
@@ -37,6 +42,29 @@ gulp.task('dev', ['watch'], function() {
 	return gulp.src(options.src + '/js/**/*.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'));
+})
+
+gulp.task('html', function() {
+    var assets = useref.assets();
+    gulp.src(options.src + '/index.html')
+        .pipe(assets)
+        .pipe(iff('*.js', uglify()))
+        .pipe(iff('*.css', csso()))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest(options.dist));
+})
+
+gulp.task('build', ['html'], function() {
+	gulp.src([options.src + '/img/**'
+			],
+		{base: options.src}
+	)
+	.pipe(gulp.dest(options.dist));
+})
+
+gulp.task('clean', function() {
+	del(['dist']);
 })
 
 
